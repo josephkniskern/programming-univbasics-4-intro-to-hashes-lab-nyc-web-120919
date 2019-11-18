ce8c20849ea955d9951715e2149887ddabf680e4
Joseph Kniskern <kniskernjoseph@yahoo.com>
2019-11-18 20:51:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,11 +31,7 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hash = {
-    "hello" => 1 
-  }
-  
-  if hash[key] && key != "hello"
+  if hash[key]
     hash[key] += 1
   else
     hash[key] = 1
